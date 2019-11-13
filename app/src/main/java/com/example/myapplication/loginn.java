f65c627373b6bbbbb71cdcdee1b529907beac2a2
Justin Robinson <robi778235@gmail.ccom>
2019-11-13 18:40:28 -0500

    Register page New Login

---
+++ app/src/main/java/com/example/myapplication/loginn.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+
+public class loginn extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogin, bSignUp;
+    EditText etusername, etpassword;
+    CheckBox etcheckbox;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginn);
+
+        etusername = (EditText) findViewById(R.id.etusername);
+        etpassword = (EditText) findViewById(R.id.etpassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        bSignUp = (Button) findViewById(R.id.bSignUp);
+        etcheckbox = (CheckBox) findViewById(R.id.etcheckbox);
+
+        bLogin.setOnClickListener(This);
+
+
+
+    }
+
+    @Override
+    public void onClick (View v) {
+        switch(v.getId()){
+            case R.id.bLogin:
+                break;
+        }
+    }
+}
